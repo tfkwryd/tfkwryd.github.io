4f83d79b96fe2b752e5e4e54de53f19d5eadeb9d
Taufik Wiryadi <taufik.wiryadi@amaan.co.id>
2022-12-05 23:52:26 +0700

    disable paralax leaf

--- assets/js/script.js
+++ assets/js/script.js
@@ -22,30 +22,30 @@
 //   }, 3000)
 // }
 
-window.addEventListener('scroll', throttle(parallax, 14))
-function throttle(fn, wait) {
-  var time = Date.now()
-  return function() {
-    if ((time + wait - Date.now()) < 0) {
-      fn();
-      time = Date.now()
-    }
-  }
-};
+// window.addEventListener('scroll', throttle(parallax, 14))
+// function throttle(fn, wait) {
+//   var time = Date.now()
+//   return function() {
+//     if ((time + wait - Date.now()) < 0) {
+//       fn();
+//       time = Date.now()
+//     }
+//   }
+// };
 
-function parallax() {
-  let scrolled = window.pageYOffset
+// function parallax() {
+//   let scrolled = window.pageYOffset
 
-  let leaf1 = 100
-  let leaf2 = 0
-  let leaf3 = -200
-  let leaf4 = 170
+//   let leaf1 = 100
+//   let leaf2 = 0
+//   let leaf3 = -200
+//   let leaf4 = 170
 
-  document.querySelector('#leaf-1').style.bottom = leaf1 + (scrolled/11) + 'px'
-  document.querySelector('#leaf-2').style.bottom = leaf2 + (scrolled/9) + 'px'
-  document.querySelector('#leaf-3').style.bottom = leaf3 + (scrolled/6) + 'px'
-  document.querySelector('#leaf-4').style.bottom = leaf4 + (scrolled/15) + 'px'
-};
+//   document.querySelector('#leaf-1').style.bottom = leaf1 + (scrolled/11) + 'px'
+//   document.querySelector('#leaf-2').style.bottom = leaf2 + (scrolled/9) + 'px'
+//   document.querySelector('#leaf-3').style.bottom = leaf3 + (scrolled/6) + 'px'
+//   document.querySelector('#leaf-4').style.bottom = leaf4 + (scrolled/15) + 'px'
+// };
 
 // window.onload = function() {
 //   setTimeout(function(){
